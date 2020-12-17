3e586e6a3a929c0cce62640e6efc42ffd3b2f5ab
Guillaume Lefay <guillaume.lefay@gmail.com>
2020-12-17 12:19:31 +0100

    model competition created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_111109) do
+ActiveRecord::Schema.define(version: 2020_12_17_111839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
