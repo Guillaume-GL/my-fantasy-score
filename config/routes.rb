3f5b505c00c02e8a6a7df244b3e41f4a3d706b48
Guillaume Lefay <guillaume.lefay@gmail.com>
2020-12-17 16:34:04 +0100

    routes démarrées mais commentées

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # resources :leagues, only: [:index, :show, :new, :create]
+  # resources :users, only: [:new, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
