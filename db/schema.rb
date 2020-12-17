307d530fbe5eb629721f6a1f0356e861eef85895
Guillaume Lefay <guillaume.lefay@gmail.com>
2020-12-17 12:37:02 +0100

    pronostics added leagues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_112951) do
+ActiveRecord::Schema.define(version: 2020_12_17_113614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_12_17_112951) do
     t.text "gain"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "pronostic_id", null: false
+    t.index ["pronostic_id"], name: "index_leagues_on_pronostic_id"
     t.index ["user_id"], name: "index_leagues_on_user_id"
   end
 
@@ -66,4 +68,5 @@ ActiveRecord::Schema.define(version: 2020_12_17_112951) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "leagues", "pronostics"
 end
