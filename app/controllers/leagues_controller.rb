9243a77a4829e34adfe684465aabe6938f96d5d1
Guillaume Lefay <guillaume.lefay@gmail.com>
2021-01-26 15:52:22 +0100

    routes leagues created, controller leagues created, pages league index, leagues show and leagues new created

---
+++ app/controllers/leagues_controller.rb
@@ -0,0 +1,15 @@
+class LeaguesController < ApplicationController
+  #skip_before_action :authenticate_user!, only: [ :home ]
+
+  def home
+    
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+end
